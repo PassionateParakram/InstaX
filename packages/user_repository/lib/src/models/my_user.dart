27ac1527b9b953b48fd0567de63cd84f89fa011d
Parakram Singh <Parakram2222@gmail.com>
2024-05-11 01:20:26 +0530

    creating user model

--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,3 +1,38 @@
-class MyUser{
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
+class MyUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUser(
+      {required this.id,
+      required this.email,
+      required this.name,
+      required this.picture});
+
+  static const empty = MyUser(id: "", email: "", name: "", picture: "");
+
+  //to modified the value we are using this
+  MyUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  }) {
+    return MyUser(
+        id: id ?? this.id,
+        email: email ?? this.email,
+        name: name ?? this.name,
+        picture: picture ?? this.picture);
+  }
+
+  bool get isEmpty => this ==MyUser.empty;
+  bool get isNotEmpty => this != MyUser.empty;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, email, name, picture ];
 }
