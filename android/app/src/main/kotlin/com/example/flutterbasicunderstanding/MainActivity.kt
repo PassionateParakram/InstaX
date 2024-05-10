bda0d6cfc7503442f2045665ae7d179bb4ce5970
Parakram Singh <Parakram2222@gmail.com>
2024-05-10 23:58:41 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/flutterbasicunderstanding/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterbasicunderstanding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterbasicunderstanding/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
